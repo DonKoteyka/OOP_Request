3aa377cb98b235bca2ecc27d7492637342521702
Don Koteyka <don.kotejka@yandex.ru>
2023-04-09 14:40:14 +0300

    Wrong commit

--- 1.py
+++ 1.py
@@ -5,9 +5,14 @@ super_hero = ['Hulk', 'Captain America', 'Thanos']
 def sh_intellegesce(url, super_hero):
     respond = requests.get(url).json()
     index_hero = [i for i in range(len(respond)) if respond[i].get('name') in super_hero]
-    most_intellegence_hero = sorted([(respond[x].get('powerstats').get('intelligence'), respond[x].get('name')) for x in index_hero], key=lambda y: y[0], reverse=True)
+    top_list = dict()
+    for x in index_hero:
+        if
+        top_list.setdefault((respond[x].get('powerstats').get('intelligence')), [respond[x].get('name')])
+        top_list[respond[x].get('powerstats').get('intelligence')].append(respond[x].get('name'))
 
-    return most_intellegence_hero[0][1]
+
+    return top_list
 pprint(sh_intellegesce(url, super_hero))
 
 
